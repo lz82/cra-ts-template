daf226d1cf520eeef552d040cdcc680bb7eaa2e0
lz82 <nickylau82@gmail.com>
2020-11-24 11:17:15 +0800

    :construction: test eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'antd';
 import './App.less';
 import css from './app.module.less';
 import MainComponent from '@/components/main';
 
-const a = 'a';
-a = 'b';
-console.log(c);
+// console.log(c);
 function App() {
+  const [size, setSize] = useState(100);
+
+  useEffect(() => {
+    setSize(1000);
+  }, []);
   return (
     <div className={css['App']}>
       <Button type="primary">add</Button>
       <MainComponent />
+      {size}
     </div>
   );
 }
